24ddced1ca14996e4f436320738e22bd41a89119
gimy boya <b.afatah@yahoo.com>
2017-05-27 20:22:30 +0800

    Brainwallet gen
    the brainwallet can be generated

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ const vm = new Vue({
   render: h => h(App),
 });
 
+console.log(NEM);
 // Set a wallet name
 const swalletName = 'Sempletest';
 
@@ -23,31 +24,31 @@ const bwalletName = 'Braintest';
 const bpassword = 'Braintest@4749';
 
 // Create Brain wallet
-const Brainwallet = NEM.model.wallet.createBrain(
-  bwalletName,
-  bpassword,
-  NEM.model.network.data.testnet.id);
+const Brainwallet = NEM.model.wallet.createBrain(bwalletName, bpassword, NEM.model.network.data.testnet.id);
 
 // Create PRNG wallet
-const Simplewallet = NEM.model.wallet.createPRNG(
-  swalletName,
-  spassword,
-  NEM.model.network.data.testnet.id);
-
+const Simplewallet = NEM.model.wallet.createPRNG(swalletName, spassword, NEM.model.network.data.testnet.id);
+console.log(Brainwallet);
+console.log(Simplewallet);
 // Create a common object
 const scommon = NEM.model.objects.create('common')('Testnet@4749', '');
 const bcommon = NEM.model.objects.create('common')('Braintest@4749', '');
 
 // Get the wallet account to decrypt
-const BwalletAccount = Simplewallet.accounts[0];
-const SwalletAccount = Brainwallet.accounts[0];
-
+const SwalletAccount = Simplewallet.accounts[0];
+const BwalletAccount = Brainwallet.accounts[0];
+// console.log(bcommon);
+// console.log(bcommon.password);
+// console.log(BwalletAccount);
+// console.log(BwalletAccount.algo);
+// console.log(!bcommon || !bcommon.password || !BwalletAccount || !BwalletAccount.algo);
 // Decrypt account private key
-NEM.crypto.helpers.passwordToPrivatekey(bcommon, bcommon.password, BwalletAccount, Brainwallet.algo);
-NEM.crypto.helpers.passwordToPrivatekey(scommon, scommon.password, SwalletAccount, Simplewallet.algo);
+const spk = NEM.crypto.helpers.passwordToPrivatekey(bcommon, BwalletAccount, Brainwallet.accounts[0].algo);
+const bpk = NEM.crypto.helpers.passwordToPrivatekey(scommon, SwalletAccount, Simplewallet.accounts[0].algo);
 
 // The common object now has a private key
 console.log(scommon);
 console.log(bcommon);
+
 
 vm.$mount(document.getElementById('app'));
