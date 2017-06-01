0ec413287597535744bc5c7c70a7e9af890a631a
gimy boya <b.afatah@yahoo.com>
2017-06-01 14:55:11 +0800

    canvas is now resized!!!

--- src/components/veditor.vue
+++ src/components/veditor.vue
@@ -1,29 +1,27 @@
 <template>
     
-<section id="editor-container">
+<section id="editor-container" v-resize:debounce="onResize">
   <canvas id="editor"></canvas>
-  <!--<el-input-number size="small" v-model="width"></el-input-number>
-  <el-input-number size="small" v-model="height"></el-input-number>
-  <el-input-number size="small" v-model="zoom"></el-input-number>-->
-
-
 </section>
 
 </template>
 
 <script>
+import resize from 'vue-resize-directive'
 import { mapActions } from 'vuex';
-
+import { EventBus } from '../assets/js/eventbus.js';
+import $ from 'jquery';
 
 
 
 
 
 export default {
+  directives: {
+    resize,
+  },
   data() {
     return {
-      cwidth: null,
-      cheight: null,
       canvas: {},
       zoom: null,
     };
@@ -55,21 +53,26 @@ export default {
     // }
   },
   methods: {
+    onResize(event){
+      this.canvas.setWidth($('#editor-container').width());
+      this.canvas.setHeight($('#editor-container').height());
+      console.log(this.canvas.getWidth());
+      console.log(this.canvas.getHeight());
+      this.canvas.viewportCenterObject(this.canvas.getObject(rect));
+    }
   },
   mounted: function () {
   
 
     const _this = this; // we save the scope
     // we get the size of the canvas container to adjust it to it
-    let width = document.getElementById('editor-container').offsetWidth;
-    let height = document.getElementById('editor-container').offsetHeight;
 
     this.canvas = new fabric.Canvas('editor');
     this.canvas.setDimensions({
-      width: width,
-      height: height,
+      width: _this.width,
+      height: _this.height,
     });
-
+    
     this.canvas.setBackgroundColor({
       source: './assets/img/opbg.jpg',
       repeat: 'repeat',
@@ -85,41 +88,7 @@ export default {
       width: 100,
       height: 100
     });
-
-    // listen to windows resize
-    window.addEventListener('resize', resizeCanvas, false);
-
-    function resizeCanvas(){
-      let newWidth = document.getElementById('editor-container').offsetWidth;
-      let newHeight = document.getElementById('editor-container').offsetHeight
-
-      let scalY = (canvas.getWidth() / canvas.getHeight()) * newHeight;
-      let scalX = (canvas.getHeight() / canvas.getWidth()) * newWidth;
-
-      // we redrwa all the objects to keep the quality
-      reDraw(scalX, scalY);
-      canvas.setWidth(canvas.getWidth() * scalX);
-      canvas.setHeight(canvas.getHeight() * scalY);
-      canvas.renderAll();
-      canvas.calcOffset();
-      
-    }
-
-    function reDraw(scalX, scalY) {
-
-      let objects = _this.canvas.getObjects();
-      for (var i in objects) {
-        objects[i].scaleX = objects[i].scaleX * scalX;
-        objects[i].scaleY = objects[i].scaleY * scalY;
-        objects[i].left = objects[i].left * scalX;
-        objects[i].top = objects[i].top * scalY;
-        objects[i].setCoords();
-      }
-      
-    }
-
-    resizeCanvas();
-   // "add" rectangle onto canvas
+    
     _this.canvas.add(rect);
     _this.canvas.viewportCenterObject(rect); 
 
